ff9727e52a9a19ce7e6d87bbed91036c25d7fb41
Mukilan Ramakrishnan <mukilanramays2007@gmail.com>
2026-04-25 14:50:55 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,17 +1,20 @@
+`default_nettype none
+
 module counter (
-    input clk,
-    input reset,
-    input up_down,
+    input  wire clk,
+    input  wire reset,    // Active-high reset
+    input  wire up_down,  // 1 for Up, 0 for Down
     output reg [7:0] count
 );
 
-always @(posedge clk) begin
-    if (reset)
-        count <= 8'b00000000;
-    else if (up_down)
-        count <= count + 1;
-    else
-        count <= count - 1;
-end
+    // Added 'posedge reset' to make it an Asynchronous Reset
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            count <= 8'b00000000;
+        else if (up_down)
+            count <= count + 1;
+        else
+            count <= count - 1;
+    end
 
 endmodule
